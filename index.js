7cabbf10dfcc1964d94d96629425d0ecf55b2d21
realshak7781 <124501803+realshak7781@users.noreply.github.com>
2024-06-17 23:51:54 +0530

    delete method added to delete a specific joke

--- index.js
+++ index.js
@@ -74,6 +74,20 @@ app.patch("/jokes/:id",(req,res)=>{
 });
 
 //7. DELETE Specific joke
+app.delete("/jokes/:id",(req,res)=>{
+  const id=parseInt(req.params.id);
+  const searchIndex=jokes.findIndex((joke)=>joke.id===id);
+
+  if(searchIndex>-1){
+    jokes.splice(searchIndex,1);
+    res.sendStatus(200);
+  }
+  else{
+    res
+    .status(404)
+    .json({error: `jokes with ${id} not found. No joke deleted`});
+  }
+});
 
 //8. DELETE All jokes
 
